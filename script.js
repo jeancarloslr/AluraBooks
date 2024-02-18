c297f72e78fedcdb6d66ad4126c4e672db444bd6
Jean Carlos <jeanxh4@gmail.com>
2024-02-18 14:51:11 -0300

    carrosel feito com biblioteca SwiperJS

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ function openHamburguer(){
     const menu = document.querySelector(".container-left");
     menu.classList.toggle('active');
 }
+
+const swiper = new Swiper('.swiper', {
+    spaceBetween: 10,
+    slidesPerView: 3,
+
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+  });
